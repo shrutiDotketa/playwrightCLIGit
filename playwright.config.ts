e03de2cfe9fa10a692c0efce0da54a48d8d15e61
shrutiDotKeta <shrutiketac@gmail.com>
2026-08-18 22:26:20 +0530

    Updated git hub workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,9 +6,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
+ reporter: [
     ['list'],
-    ['html', { open: 'never' }],
+    ['html', { outputFolder: 'playwright-report' }],
+    ['junit', { outputFile: 'test-results/e2e-results.xml' }],
     ['allure-playwright'],
   ],
   use: {
